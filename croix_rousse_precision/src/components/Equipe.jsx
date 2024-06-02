fe2d220cb02d77df8d4b46d14ece3b48d2d22e94
Keenan MARTIN <kesmartin2004@gmail.com>
2024-06-02 19:22:46 +0200

    Fixe error

--- croix_rousse_precision/src/components/Equipe.jsx
+++ croix_rousse_precision/src/components/Equipe.jsx
@@ -4,7 +4,7 @@ const Equipe = () => (
   <section
     className="w-screen sm:fixed sm:-top-5 sm:left-0 sm:w-full sm:z-[-1]" // Apply the new class here
   >
-    <img src={EquipePhoto} />
+    <img className="w-screen" src={EquipePhoto} />
   </section>
 );
 
